1eb86ee404b24ea518911770bc40f1db99752ee4
bughh <bugphh@gmail.com>
2024-02-11 23:56:12 +0800

    添加 put_int

--- c6/impl_print/kernel/src/main.c
+++ c6/impl_print/kernel/src/main.c
@@ -1,9 +1,10 @@
 int main() {
-    put_char('P');
-  	put_char('H');
-    put_char('H');
-  	put_char('\n');
-    put_str("Hello, Apple");
+  put_char('P');
+  put_char('H');
+  put_char('H');
+  put_char('\n');
+  put_str("Hello, Apple\n");
+  put_int(16702650);
   while (1);
   return 0;
 }
