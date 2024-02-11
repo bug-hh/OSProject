8f3e31c701e5c1f30cbd84b65d31f652d3f23773
bughh <bugphh@gmail.com>
2024-02-12 00:43:27 +0800

    bug fix put_int_buffer

--- c6/impl_print/kernel/src/main.c
+++ c6/impl_print/kernel/src/main.c
@@ -1,9 +1,11 @@
+#include "print.h"
 int main() {
   put_char('P');
   put_char('H');
   put_char('H');
   put_char('\n');
-  put_str("Hello, Apple\n");
+  put_str("Hello, Apple");
+  put_char('\n');
   put_int(16702650);
   while (1);
   return 0;
