a2875eedb2ec78400b7ff60facc721e7cc23e571
bughh <bugphh@gmail.com>
2024-02-11 12:33:00 +0800

    完成加载内核

---
+++ c5/load_kernel/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+nasm -I ./boot/src/include/ -o ./boot/bin/mbr.bin boot/src/mbr.S
+nasm -I ./boot/src/include/ -o ./boot/bin/loader.bin boot/src/loader.S
+
+dd if=./boot/bin/mbr.bin of=../../hd60M.img bs=512 count=1 seek=0 conv=notrunc
+dd if=./boot/bin/loader.bin of=../../hd60M.img bs=512 count=3 seek=2 conv=notrunc
+
+# use 32 bit
+# nasm -f elf -o lib/kernel/print.o lib/kernel/print.S
+
+gcc -m32 -c -o kernel/obj/main.o kernel/src/main.c
+
+ld -m elf_i386 -Ttext 0xc0001500 -e main -o kernel/bin/kernel.bin kernel/obj/main.o
+
+dd if=kernel/bin/kernel.bin of=../../hd60M.img bs=512 count=200 seek=9 conv=notrunc
+
+bochs -f ../../bochsrc.disk
